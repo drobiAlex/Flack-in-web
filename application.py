57caf7a8365bd84f85d2c1993df88d2da9dc149d
drobiAlex <amperescom@gmail.com>
2019-04-06 17:36:20 +0200

    Display Name

--- application.py
+++ application.py
@@ -1,13 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+users = []
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+	return render_template("index.html")
+
+@socketio.on("new_user")
+def connect(user):
+	if user not in users:
+		abort(403)
+	else:
+		users.appent(user)
---
+++ static/index.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Ensure if user has a nickname
+	if (localStorage.getItem('myNickname') == null){
+
+
+		document.querySelector('#new-name').onsubmit = () => {
+
+			// Create a new variable with nickname from form
+			let new_name = document.querySelector('#input_name').value;
+
+			// Print output to the log
+			console.log('------');
+			console.log(new_name);
+			console.log('------');
+
+			document.querySelector('#name').innerHTML = new_name;
+
+			// Save nickname in browser
+			localStorage.setItem('myNickname', new_name);
+
+			// Remove nickname form
+			document.querySelector('#new-name').remove();
+			return false;
+		}
+	}
+
+	// Set user nickname
+	else {
+
+		//
+		document.querySelector('#name').innerHTML = localStorage.getItem('myNickname');
+
+		// Remove nickname form
+		document.querySelector('#new-name').remove();
+
+		setTimeout(function () {
+
+		// Will redirect to your blog page (an ex: blog.html)
+        window.location.href = "chats.html";
+
+		// Will call the function after 2 secs.
+    	}, 2000);
+
+		return false;
+	}
+
+});
+
+function clr() {
+
+	localStorage.clear();
+	document.location.reload()
+}
